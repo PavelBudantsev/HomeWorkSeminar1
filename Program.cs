1964237503db7a7d21ee12720670e2cc3ab6095e
Pavel_Budantsev <p.budantsev@ya.ru>
2022-08-05 15:05:52 +0500

    add task 3

--- Program.cs
+++ Program.cs
@@ -44,3 +44,16 @@
 
 // Console.WriteLine("Наибольшее число из них: " + max);
 
+// на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Console.Write("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 2 == 0)
+// {
+//     Console.WriteLine("Это число четное");
+// }
+// else
+// {
+//     Console.WriteLine("Это число нечетное");
+// }
+
