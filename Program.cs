2ce54f23b7e8e868272e2a56e5cc1d1337b85132
Pavel_Budantsev <p.budantsev@ya.ru>
2022-08-05 14:56:54 +0500

    add task 2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,46 @@
 ﻿// Задача 2. Напишите программу, которая на вход принимает два числа 
 // и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите первое число ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber > secondNumber)
-{
-    Console.WriteLine("Наибольшее число из них: " + firstNumber);
-}
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine("Наибольшее число из них: " + firstNumber);
+// }
 
-if (secondNumber > firstNumber)
-{
-    Console.WriteLine("Наибольшее число из них: " + secondNumber);
-}
+// if (secondNumber > firstNumber)
+// {
+//     Console.WriteLine("Наибольшее число из них: " + secondNumber);
+// }
 
-if (firstNumber == secondNumber)
-{
-    Console.WriteLine("Эти числа равны");
-}
+// if (firstNumber == secondNumber)
+// {Console.WriteLine("Наибольшее число из них: " + secondNumber);
+//     Console.WriteLine("Эти числа равны");
+// }
+
+//Задача 4. принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+// int max = firstNumber;
+
+// if (secondNumber > max)
+// {
+//     max = secondNumber;
+// }
+
+// if (thirdNumber > max)
+// {
+//     max = thirdNumber;
+// }
+
+// Console.WriteLine("Наибольшее число из них: " + max);
+
